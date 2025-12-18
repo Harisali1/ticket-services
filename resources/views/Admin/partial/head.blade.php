abac79723bf7c752a66ce59a69320f93c7912e33
Haris-Ali-90 <muhammad.haris@leopardscourier.com>
2025-12-19 02:11:15 +0500

    tailwind

--- resources/views/Admin/partial/head.blade.php
+++ resources/views/Admin/partial/head.blade.php
@@ -2,7 +2,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>@yield('title')</title>
-  <script src="{{ asset('build/css/tailwind.css') }}"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+  <!-- <script src="{{ asset('build/css/tailwind.css') }}"></script> -->
   <!-- <script src="https://unpkg.com/alpinejs" defer></script> -->
   <link href="{{ asset('vendors/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
   <meta name="csrf-token" content="{{ csrf_token() }}">
